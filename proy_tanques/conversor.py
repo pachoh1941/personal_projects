d8405642bfc7904d55b643b3f53203cd3ccf78fd
Juan F. Hurtado <jf.hurtado10@uniandes.edu.co>
2022-04-23 16:17:09 -0500

    Se crea la clase Conversor y se guarda el script de Tanque. Queda pendiente corregir los métodos de Conversor a uno solo de conversión y que el dict de unidades sea dict de pequeño arrays de conversión

---
+++ proy_tanques/conversor.py
@@ -0,0 +1,23 @@
+
+class Conversor():
+
+    def __init__(self, tipo_de_magnitud, uns_entrada, uns_salida):
+        self.tipo_de_magnitud = tipo_de_magnitud
+        self.uns_entrada = uns_entrada #Unidades de entrada
+        self.uns_salida = uns_salida #Unidades de salida
+        self.unidades = {} #Diccionario que almacena el conjunto de unidades
+
+    def bar_a_psi():
+        pass
+
+    def in_a_mm():
+        pass
+
+    def gal_a_L():
+        pass
+
+    def L_a_mm():
+        pass
+
+    def agregar_unidad(nueva_un_entrada, nueva_un_salida): #permite al usuario agregar una nueva conversión de unidades
+        pass
