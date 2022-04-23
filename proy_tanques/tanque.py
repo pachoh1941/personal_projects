91672a5334e54bd7abe134edbadecc2cadf680d6
Juan F. Hurtado <jf.hurtado10@uniandes.edu.co>
2022-04-23 16:17:59 -0500

    Timestamp de tanque.py

---
+++ proy_tanques/tanque.py
@@ -0,0 +1,9 @@
+
+class Tanque():
+
+    def __init__(self, volumen, mawp, presion_diseno):
+        self.volumen = volumen
+        self.mawp = mawp # Presión máxima permitida - Maximum Allowable Wall Pressure
+        self.presion_diseno = presion_diseno
+
+    
