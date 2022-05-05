0f611dd2b196dfba2fcc8acaf0fd02c9200966d5
Juan F. Hurtado <jf.hurtado10@uniandes.edu.co>
2022-05-05 07:07:15 -0500

    Se crea el esqueleto del script de selección

---
+++ proy_tanques/seleccionador.py
@@ -0,0 +1,27 @@
+
+def seleccionar_por_arranque():
+    #funcion que selecciona tanque por tiempo de arranque de compresor
+    #asume los parámetros que no son ingresados en el array de especs
+    pass
+
+def seleccionar_por_vaciado():
+    #funciona que selecciona tanque por tiempo de vaciado
+    #asume los parámetros que no son ingresados en el array de especs
+    pass
+
+def seleccionar_por_llenado():
+    #funciona que selecciona tanque por tiempo de llenado
+    #asume los parámetros que no son ingresados en el array de especs
+    pass
+
+def seleccionar_tanque(especificaciones, criterio):
+    #funcion que gestiona la selección según un array de especificaciones y un criterio
+    #se debe deifnir un orden del array (puede ser según cuaderno)
+    pass
+
+if __name__== '__main__':
+
+    especificaciones = []
+    criterio = int(input('¿Bajo qué criterio seleccionarás el tanque? A, B o C'))
+
+    seleccionar_tanque(especificaciones, criterio)
